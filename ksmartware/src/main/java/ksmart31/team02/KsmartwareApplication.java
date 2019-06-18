37d8939c3aae8873aee03d3509e58c4ba76d3cee
smart-202-21 <smart-202-21@smart-202-21-PC>
2019-06-18 15:15:21 +0900

    2019-06-18
    유형:
    팀프로젝트 레파지토리 테스트
    
    
    본문 :
    프로젝트 명 Spring boot를 기반으로 Google API를 활용한 그룹웨어 프로젝트

---
+++ ksmartware/src/main/java/ksmart31/team02/KsmartwareApplication.java
@@ -0,0 +1,13 @@
+package ksmart31.team02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsmartwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsmartwareApplication.class, args);
+	}
+
+}
