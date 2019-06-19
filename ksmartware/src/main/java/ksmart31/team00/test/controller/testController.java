8dce1e1dcaec443e31ddd3281f17264e01729763
PC <PC@ZEUS08>
2019-06-19 20:39:52 +0900

    [2019-06-19] 커밋 테스트
    부트스트랩 템플릿  파일 넣어놓음
    필요한 예제파일 사용해서 프론트 제작할 것
    
    정환이 로그인 작업해둔 것 부트스트랩에 적용함

--- ksmartware/src/main/java/ksmart31/team00/test/controller/testController.java
+++ ksmartware/src/main/java/ksmart31/team00/test/controller/testController.java
@@ -5,9 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class testController {
-	@GetMapping(value = { "/", "/index" })
+
+	@GetMapping(value = "/test")
 	public String index() {
 		System.out.println("IndexController.index()");
 		return "test";
+	}
+		
+	@GetMapping(value = "/calendar")
+	public String calendar() {
+		System.out.println("IndexController.calendar()");
+		return "/pages/calendar";
+	}	
+			
+	@GetMapping("/layouts")
+	public String layouts(){
+		
+		return "pages/layouts";
 	}
 }
---
+++ ksmartware/src/main/java/ksmart31/team01/member/mapper/MemberLoginMapper.java
@@ -0,0 +1,14 @@
+package ksmart31.team01.member.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import ksmart31.team01.member.domain.Member;
+
+@Mapper
+public interface MemberLoginMapper {
+	public Member selectMemberForLogin(Member member); //로그인 처리를 위한 회원 정보 조회
+
+
+
+
+}
