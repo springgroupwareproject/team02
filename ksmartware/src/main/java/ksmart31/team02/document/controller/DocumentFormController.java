101742bef95beede1a86192a4fce165ce428a171
Administrator <Administrator@MSDN-SPECIAL>
2019-06-24 16:33:21 +0900

    [190621] admin에 들어있는 파일들 경로 이동 사용자 문서양식 조회 휴가신청서 작성폼 지출결의서 작성폼
    [190624]
    프로젝트 지출결의서 작성폼
    프로젝트 업무보고 작성폼
    구매요청서 작성폼

--- ksmartware/src/main/java/ksmart31/team02/document/controller/DocumentFormController.java
+++ ksmartware/src/main/java/ksmart31/team02/document/controller/DocumentFormController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import ksmart31.team01.member.mapper.MemberLoginMapper;
 import ksmart31.team02.document.mapper.DocumentFormMapper;
 import ksmart31.team02.document.vo.DocumentFormCategory;
 
@@ -15,18 +14,32 @@ import ksmart31.team02.document.vo.DocumentFormCategory;
 public class DocumentFormController {
 	@Autowired private DocumentFormMapper documentFormMapper;
 	
+	// 구매요청서 작성폼
+	@GetMapping("/purchaseRequisitionForm")
+	public String addPurchaseRequisitionForm() {
+		System.out.println("(C) addPurchaseRequisitionForm() 실행");
+		return "member/documentForm/purchaseRequisitionForm";
+	}
+	
+	// 프로젝트 업무보고 작성폼
+	@GetMapping("/projectReportForm")
+	public String addProjectReportForm() {
+		System.out.println("(C) addProjectReportForm() 실행");
+		return "member/documentForm/projectReportForm";
+	}
+	
+	// 프로젝트 지출결의서 작성폼
+	@GetMapping("/projectCashDisbursementVoucherForm")
+	public String addProjectCashDisbursementVoucherForm() {
+		System.out.println("(C) addProjectCashDisbursementVoucherForm() 실행");
+		return "member/documentForm/projectCashDisbursementVoucherForm";
+	}
+	
 	// 지출결의서 작성폼
 	@GetMapping("/cashDisbursementVoucherForm")
 	public String addCashDisbursementVoucherForm() {
 		System.out.println("(C) addCashDisbursementVoucherForm() 실행");
 		return "member/documentForm/cashDisbursementVoucherForm";
-	}
-	
-	// 휴가신청서 작성폼
-	@GetMapping("/leaveApplicationForm")
-	public String getLeaveApplicationForm(Model model) {
-		System.out.println("(C) getLeaveApplicationForm() 실행");
-		return "member/documentForm/leaveApplicationForm";
 	}
 	
 	// 공통양식 카테고리 목록
@@ -38,4 +51,16 @@ public class DocumentFormController {
 		model.addAttribute("list",list);
 		return "member/draftDocument/documentFormList";
 	}
+	
+	//test
+	// 휴가신청서 작성폼
+	@GetMapping("/leaveApplicationForm")
+	public String getLeaveApplicationForm(String documentFormCode, Model model) {
+		System.out.println("(C) getLeaveApplicationForm() 실행");
+		System.out.println("(C) getLeaveApplicationForm() documentFormCode:"+documentFormCode);
+		
+		return "member/documentForm/leaveApplicationForm";
+	}
+	
+	
 }
--- ksmartware/src/main/java/ksmart31/team02/document/service/DocumentFormService.java
+++ ksmartware/src/main/java/ksmart31/team02/document/service/DocumentFormService.java
@@ -5,13 +5,13 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import ksmart31.team01.member.mapper.MemberLoginMapper;
+//import ksmart31.team01.member.mapper.MemberLoginMapper;
 import ksmart31.team02.document.mapper.DocumentFormMapper;
 
 @Service
 public class DocumentFormService {
 	@Autowired private DocumentFormMapper documentFormMapper;
-	@Autowired private MemberLoginMapper memberLoginMapper;
+	//@Autowired private MemberLoginMapper memberLoginMapper;
 	
 	// 휴가신청서 작성폼
 	public Map<String, Object> getLeaveApplicationForm() {
